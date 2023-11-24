ff60c878096d91cd82e0aa99446fdb9419c2d94d
John Mark Delima <delimajohnmark14@gmail.com>
2023-11-24 22:41:38 +0800

    Update script.js

--- plants-landing-page/script.js
+++ plants-landing-page/script.js
@@ -1,6 +1,6 @@
 // Get multiple elements instead of a single one using "querySelectorAll"
 const leafs = document.querySelectorAll('.animated-element');
-            
+const ascend = document.querySelectorAll('.animate-ascend');          
 // Create the observer like the examples above
 const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
@@ -9,13 +9,19 @@ const observer = new IntersectionObserver(entries => {
             leafs.forEach(leaf => {
                 leaf.classList.add('animate-fall');
             });
+            ascend.forEach(e => {
+                e.classList.add('animate-ascend');
+            });
         } else {
             // Loop through each leaf and remove the class individually
-            leafs.forEach(leaf => {
-                leaf.classList.remove('animate-fall');
+            leafs.forEach(e => {
+                e.classList.remove('animate-fall');
+            });
+            ascend.forEach(e => {
+                e.classList.remove('animate-ascend');
             });
         }
     });
 });
 
-observer.observe(document.querySelector('.plants-wrapper'));
+observer.observe(document.querySelector('.page-1'));
